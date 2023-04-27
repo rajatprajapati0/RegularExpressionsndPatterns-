3227dd3692394d3ec3f15b95187794bce32eefda
Rajat <rajatprajapati09@gmail.com>
2023-04-28 00:31:46 +0530

    [rajat]program that matches a string that has an ‘a’ followed by two to three 'b'.

---
+++ MatcheString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressionsAndPatterns
+{
+    public class pattern
+    {
+        public static string Strings = "^[a]{1}[b]{2,}$";
+    }
+    internal class MatcheString
+    {
+
+        public void  MatcheStrings()
+        {
+
+            Regex str = new Regex(pattern.Strings);
+            bool check = false;
+           string inputStr= Console.ReadLine();
+            check = str.IsMatch(inputStr);
+
+            if (check==true)
+            {
+                Console.WriteLine("match");
+
+            }
+            else 
+            {
+                Console.WriteLine("notMatch");
+            }
+        }   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RegularExpressionsAndPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MatcheString matcheString = new MatcheString();
+            matcheString.MatcheStrings();
+        }
+    }
+}
